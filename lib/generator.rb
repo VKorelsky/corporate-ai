1a1ce1d63fe987586873e95b0bd7f0d05d3ab121
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-10-22 21:44:42 -0400

    99% done

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,19 +1,18 @@
 require 'CSV'
 
 class Generator
-  attr_reader :adverbs
-
+  
   def initialize
     @csv = CSV.read('db/words.csv')
     remove_nil
-    @adverbs = csv[0]
-    @verbs = csv[1]
-    @adjectives = csv[2]
-    @nouns = csv[3]
+    @adverbs = @csv[0]
+    @verbs = @csv[1]
+    @adjectives = @csv[2]
+    @nouns = @csv[3]
   end
 
   def new_sentence
-     sentence = @adverbs.sample + @verbs.sample + @adjectives.sample + @nouns.sample
+     sentence = "#{@adverbs.sample } #{@verbs.sample} #{@adjectives.sample} #{@nouns.sample}"
      puts sentence
   end
 
@@ -23,6 +22,3 @@ class Generator
     @csv.each{|array| array.reject! {|value| value == nil}}
   end
 end
-
-test = Generator.new
-p test.adverbs
