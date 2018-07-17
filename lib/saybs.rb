7e747fec13db05055aa30091c189c6bf335c4f75
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2018-07-17 17:25:51 -0400

    fix

--- lib/saybs.rb
+++ lib/saybs.rb
@@ -5,7 +5,8 @@ module SayBs
   class App
     def self.run
       # entrypoint
-      csv_path = File.expand_path('db/words.csv', __FILE__)
+      csv_path = File.join(__dir__, 'db/words.csv')
+
       corpo_bs = Generator.new(csv_path).new_sentence
 
       puts corpo_bs
