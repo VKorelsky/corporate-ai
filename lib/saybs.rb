bec4b35813a0b059e211edb5089b52ba9a716ad7
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2018-07-17 00:28:20 -0400

    90% done, missing cabling for csv file

--- lib/saybs.rb
+++ lib/saybs.rb
@@ -6,7 +6,7 @@ module SayBs
     def self.run
       # entrypoint
       csv_path = File.expand_path('db/words.csv', __FILE__)
-      corpo_bs = SayBs::Generator(csv_path).new.new_sentence
+      corpo_bs = Generator.new(csv_path).new_sentence
 
       puts corpo_bs
       %x(say #{corpo_bs})
